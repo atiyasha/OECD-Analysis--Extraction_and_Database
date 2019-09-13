743e0ad26cdcab60dbb50d8d706d4bd45e0c9f6f
akaur1 <akaur1@lenovo.com>
2019-09-12 23:40:06 -0400

    started the basics of making a connection with OECD data

--- OECD_Analysis/OECD_Client.py
+++ OECD_Analysis/OECD_Client.py
@@ -1,2 +1,16 @@
 ## Print out all of the series numbers in the JSON file
-# https://www.unixmen.com/write-a-simple-client-in-python/
+# https://realpython.com/python-requests/
+
+import requests
+
+response = requests.get('https://stats.oecd.org/SDMX-JSON/data/GENDER_ENT1')
+
+if response.status_code==200:
+    print('Success!')
+else:
+    print('An error has occurred ' + str(response.status_code))
+
+oecd_json = response.json()
+
+for dataSets in oecd_json:
+
