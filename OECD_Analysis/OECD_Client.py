44fa0cd3b9995530d7d90bfef8c36e3dd26a9583
akaur1 <akaur1@lenovo.com>
2019-09-23 09:35:41 -0400

    used series key and value dictionary to print description of statistic

--- OECD_Analysis/OECD_Client.py
+++ OECD_Analysis/OECD_Client.py
@@ -6,20 +6,15 @@ import requests
 
 response = requests.get('https://stats.oecd.org/SDMX-JSON/data/GENDER_ENT1')
 
-if response.status_code==200:
+# validate that program is successfully receiving data
+if response.status_code == 200:
     print('Success!')
 else:
     print('An error has occurred ' + str(response.status_code))
 
 oecd_json = response.json()
 
-for data_set in oecd_json["dataSets"]:
-    series = data_set["series"]
-    for series_key in series.keys():
-        print(series_key)
-
 # array (key position) of an array (values) of dictionaries (id and name)
-
 structure_dimension= []
 for series in oecd_json["structure"]["dimensions"]["series"]:
     #structure_dimension[series["keyPosition"]] = series["values"] <-- not doing this way because we want to do this manually
@@ -34,3 +29,20 @@ print(str(structure_dimension[0]))
 print(str(structure_dimension[1]))
 print(str(structure_dimension[2]))
 print(str(structure_dimension[3]))
+
+
+# store all series keys
+series_position = []
+for data_set in oecd_json["dataSets"]:
+    series = data_set["series"]
+    for series_key in series.keys():
+        series_key_split_arr = series_key.split(':')
+        #print(str(series_key_split_arr))
+        country_index = int(series_key_split_arr[0])
+        indicator_index = int(series_key_split_arr[1])
+        gender_index = int(series_key_split_arr[2])
+        age_index = int(series_key_split_arr[3])
+        print(structure_dimension[0][country_index]["name"] + ", " + structure_dimension[1][indicator_index]["name"] + ", " + structure_dimension[2][gender_index]["name"] + ", " + structure_dimension[3][age_index]["name"])
+
+
+
